7288a72e78f32b02ecbd824ef38187fea916fbb7
MurylloEx <muryllopimenta@gmail.com>
2021-07-30 14:02:58 -0300

    :bookmark: release: First stable version 1.2-SNAPSHOT.

---
+++ src/main/java/br/com/muryllo/jmailer/Envelope.java
@@ -0,0 +1,68 @@
+package br.com.muryllo.jmailer;
+
+import java.util.List;
+
+public class Envelope {
+  
+  private String api_key;
+  private List<String> to;
+  private String sender;
+  private String subject;
+  private String text_body;
+  private String html_body;
+
+  public String getApiKey() {
+    return api_key;
+  }
+
+  public Envelope setApiKey(String api_key) {
+    this.api_key = api_key;
+    return this;
+  }
+
+  public List<String> getTo() {
+    return to;
+  }
+
+  public Envelope setTo(List<String> to) {
+    this.to = to;
+    return this;
+  }
+  
+  public String getSender() {
+    return sender;
+  }
+
+  public Envelope setSender(String sender) {
+    this.sender = sender;
+    return this;
+  }
+
+  public String getSubject() {
+    return subject;
+  }
+
+  public Envelope setSubject(String subject) {
+    this.subject = subject;
+    return this;
+  }
+
+  public String getTextBody() {
+    return text_body;
+  }
+
+  public Envelope setTextBody(String text_body) {
+    this.text_body = text_body;
+    return this;
+  }
+
+  public String getHtmlBody() {
+    return html_body;
+  }
+
+  public Envelope setHtmlBody(String html_body) {
+    this.html_body = html_body;
+    return this;
+  }
+
+}
---
+++ src/main/java/br/com/muryllo/jmailer/Mailer.java
@@ -0,0 +1,127 @@
+package br.com.muryllo.jmailer;
+
+import java.util.ArrayList;
+import java.util.Hashtable;
+import java.util.Map;
+
+import okhttp3.RequestBody;
+import okhttp3.MediaType;
+
+public class Mailer {
+  
+  private static final String SMTP_API_URL = "https://api.smtp2go.com/v3/";
+  private static final MediaType JSON = MediaType.parse("application/json; charset=utf-8");
+
+  private String m_SecretKey = "";
+  private String m_SenderName = "";
+  private String m_SenderEmail = "";
+  private ArrayList<String> m_Recipients = new ArrayList<>();
+  private String m_SubjectText = "";
+  private String m_TextBody = "";
+  private String m_HtmlBody = "";
+
+  /**
+   * Define a chave da api de envio de emails.
+   * 
+   * @param seckey A chave da API SMTP2GO usada para enviar os emails.
+   * @return A instância do Mailer.
+   */
+  public Mailer key(String seckey){
+    this.m_SecretKey = seckey;
+    return this;
+  }
+
+  /**
+   * Define quem enviou o email.
+   * Perceba que essa campo pode ser forjado, você pode enviar o email sendo quem quiser.
+   * 
+   * @param senderName O nome do remetente.
+   * @param senderEmail O eendereço de email do remetente
+   * @return A instância do Mailer.
+   */
+  public Mailer from(String senderName, String senderEmail){
+    this.m_SenderName = senderName;
+    this.m_SenderEmail = senderEmail;
+    return this;
+  }
+
+  /**
+   * Define para quem será enviado o email.
+   * 
+   * @param recipientName O nome do destinatário.
+   * @param recipientEmail O endereço de email do destinatário.
+   * @return A instância do Mailer.
+   */
+  public Mailer to(String recipientName, String recipientEmail){
+    this.m_Recipients.add(
+      String.format("%s <%s>", recipientName, recipientEmail));
+    return this;
+  }
+
+  /**
+   * Define o título do email (assunto).
+   * 
+   * @param text O título do email que aparecerá para o destinatário.
+   * @return A instância do Mailer.
+   */
+  public Mailer subject(String text){
+    this.m_SubjectText = text;
+    return this;
+  }
+
+  /**
+   * Define o corpo do texto do email a ser enviado.
+   * 
+   * @param text O corpo do texto do email.
+   * @return A instância do Mailer.
+   */
+  public Mailer textBody(String text){
+    this.m_TextBody = text;
+    return this;
+  }
+
+  /**
+   * Define o corpo HTML do email a ser enviado.
+   * É possível enviar emails estilizados com html e css.
+   * 
+   * @param html O corpo HTML do email. Ele deve ser feito em padrão HTML5.
+   * @return A instância do Mailer.
+   */
+  public Mailer htmlBody(String html){
+    this.m_HtmlBody = html;
+    return this;
+  }
+
+  /**
+   * Envia o email para o destinatário solicitado de forma síncrona.
+   * 
+   * @return Retorna a resposta do servidor SMTP em formato JSON.
+   */
+  public String send(){
+    try{
+      Envelope envelope = new Envelope()
+        .setApiKey(this.m_SecretKey)
+        .setTo(this.m_Recipients)
+        .setSender(String.format("%s <%s>", this.m_SenderName, this.m_SenderEmail))
+        .setSubject(this.m_SubjectText)
+        .setTextBody(this.m_TextBody)
+        .setHtmlBody(this.m_HtmlBody);
+      
+      String envelopeJson = JsonService.stringify(envelope, Envelope.class);
+      RequestBody reqBody = RequestBody.create(envelopeJson, JSON);
+  
+      Map<String, String> queryString = new Hashtable<String, String>();
+      Map<String, String> headers = new Hashtable<String, String>();
+      headers.put("Content-Type", "application/json; charset=utf-8");
+    
+      return RequestService.post(
+        SMTP_API_URL + "/email/send", 
+        reqBody, 
+        headers, 
+        queryString);
+    } catch(Exception exception){
+      return null;
+    }
+  }
+
+}
